de259678cf1fb80cc605069e4fd674f9ece609ac
saipraneeth1001 <saipraneeth0448@gmail.com>
2021-03-12 16:05:32 +0530

    how to use event emitter

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const tutorial = require('./tutorial')
-console.log(tutorial.sum(2,3));
-console.log(tutorial.pi);
-console.log(new tutorial.SomeClass())
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial',()=>{
+    console.log("new event is created");
+})
+
+eventEmitter.emit('tutorial');
+
+class Person extends EventEmitter {
+
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name; 
+    }
+}
+
+let praneeth = new Person("praneeth");
+
+praneeth.on('name',(sentence)=>{
+    console.log("hi praneeth, congrats! "+sentence);
+})
+
+praneeth.emit('name', "you are great!");
